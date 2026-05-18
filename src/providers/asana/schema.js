96d77ba76689cbd3833719cca2774766de2dbb06
Marco Moauro <marco.moauro@translated.net>
2026-05-18 21:56:28 +0200

    feat(asana): schema constants for sections and custom fields

---
+++ src/providers/asana/schema.js
@@ -0,0 +1,32 @@
+export const SECTION_TEAM_ASSIGNMENT = 'Team Assignment';
+
+export const SECTION_BY_SEVERITY = {
+  CRITICAL: 'Critical',
+  HIGH: 'High',
+  MEDIUM: 'Medium',
+  LOW: 'Low',
+};
+
+export const FIELD = {
+  DEDUP: 'Deduplication ID',
+  SEVERITY: 'Severity',
+  REPOSITORY: 'Repository',
+  PACKAGE: 'Package',
+  ADVISORY: 'Advisory',
+  ADVISORY_URL: 'Advisory URL',
+  TECH_TEAM: 'Tech Team',
+};
+
+export const SEVERITY_ENUM_OPTIONS = [
+  { name: 'Critical', color: 'red' },
+  { name: 'High', color: 'orange' },
+  { name: 'Medium', color: 'yellow' },
+  { name: 'Low', color: 'cool-gray' },
+];
+
+export const SEVERITY_TO_OPTION_NAME = {
+  CRITICAL: 'Critical',
+  HIGH: 'High',
+  MEDIUM: 'Medium',
+  LOW: 'Low',
+};
